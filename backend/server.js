c22bae61b50a3047897892af24c7e868323ca84e
jatothdialpad <jatoth.rajkumar@dialpad.com>
2023-10-20 13:20:31 +0530

    first changes

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=express()
+
+const port=process.env.PORT || 4000
+
+app.get("/",(req,res)=>{
+    res.send("hello dialpad");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`)
+})
